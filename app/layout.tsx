a6f096a346ab37c9ea1cc19293a16ffb8c9c5a87
Dávid Lévai <david@davidlevai.com>
2023-06-14 20:03:07 +0100

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,37 @@
+import "./globals.css";
+
+import { Inter, Space_Grotesk } from "next/font/google";
+
+import siteMetadata from "@/lib/metadata";
+import { Toaster } from "@/components/ui/toaster";
+import { ThemeProvider } from "@/components/theme-provider";
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-space",
+  weight: ["400", "600", "700"],
+});
+const inter = Inter({ subsets: ["latin"], display: "swap", variable: "--font-inter" });
+
+export const metadata = {
+  title: siteMetadata.title,
+  description: siteMetadata.description,
+};
+
+interface RootLayoutProps {
+  children: React.ReactNode;
+}
+
+export default function RootLayout({ children }: RootLayoutProps) {
+  return (
+    <html lang="en" className={`${spaceGrotesk.variable} ${inter.variable}`} suppressHydrationWarning>
+      <body className="min-h-screen bg-gradient-to-b from-slate-100 to-white text-slate-900 antialiased dark:bg-gradient-to-b dark:from-gray-900 dark:to-gray-800 dark:text-slate-50">
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          {children}
+          <Toaster />
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
